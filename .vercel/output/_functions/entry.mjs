e0e950e69e6cbed3bf0a12f4f648ef309fced464
ElectrEngRubenVera (Github) <niluquis1@gmail.com>
2025-03-04 11:33:24 -0500

    Se genero el build

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,36 @@
+import { renderers } from './renderers.mjs';
+import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
+import { c as createExports } from './chunks/entrypoint_BWjV4JOK.mjs';
+import { manifest } from './manifest_BmaNlm1U.mjs';
+
+const serverIslandMap = new Map([
+	['BookScore', () => import('./chunks/BookScore_C7crEDv6.mjs')],
+	['BuyButton', () => import('./chunks/BuyButton_DR9suHEw.mjs')],
+]);;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/about.astro.mjs');
+const _page2 = () => import('./pages/libro/_id_.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/.pnpm/astro@5.4.2_jiti@2.4.2_ligh_454f587c206c2d9b617b69ad121012ce/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/about.astro", _page1],
+    ["src/pages/libro/[id].astro", _page2],
+    ["src/pages/index.astro", _page3]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "14be1252-dec7-40b8-a4ed-45609f0e39b8",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
