812dcf2f7e68ef6ee1e696d2ab4c21c9c3e6f6d2
ElectrEngRubenVera <niluquis1@gmail.com>
2025-03-16 00:16:11 -0500

    a nice about is added

--- .vercel/output/functions/_render.func/entry.mjs
+++ .vercel/output/functions/_render.func/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { c as createExports } from './chunks/entrypoint_BWjV4JOK.mjs';
-import { manifest } from './manifest_Cq5lZyKA.mjs';
+import { manifest } from './manifest_JToOcV2_.mjs';
 
 const serverIslandMap = new Map([
 	['BookScore', () => import('./chunks/BookScore_aPyTH0HP.mjs')],
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "b123837f-3f95-491c-8dfb-a27504ef9556",
+    "middlewareSecret": "6244ca2c-9614-4cf8-a42b-a7fbf9ad7c4a",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
